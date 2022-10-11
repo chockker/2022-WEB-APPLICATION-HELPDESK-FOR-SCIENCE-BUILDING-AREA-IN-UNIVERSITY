c5b0101d99ee34905a1d307fb14444e604cd70f4
Boonparit <chockker555@hotmail.com>
2022-10-11 21:19:21 +0700

    update

--- application/controllers/TnJob.php
+++ application/controllers/TnJob.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class TnJob extends CI_Controller {
+class Tnjob extends CI_Controller {
 
 	public function __construct()
 	{
