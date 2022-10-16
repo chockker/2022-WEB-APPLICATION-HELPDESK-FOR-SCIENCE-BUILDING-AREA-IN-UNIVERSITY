72d8c5a72c3632a1423d582f021430a2d6868724
Boonparit <chockker555@hotmail.com>
2022-10-16 19:54:07 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/helpdesk/';
-//$config['base_url'] = 'https://project-helpdesk.herokuapp.com/';
+//$config['base_url'] = 'http://127.0.0.1/helpdesk/';
+$config['base_url'] = 'https://project-helpdesk.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
